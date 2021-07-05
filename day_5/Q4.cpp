d757890b0a2647b30f2b18fc8b1748e77e007abe
Raj-bisht <61263677+Raj-bisht@users.noreply.github.com>
2021-07-05 19:22:00 +0530

    unique morse code words

---
+++ day_5/Q4.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int i,j,n;
+    cout<<"Enter the size :";
+    cin>>n;
+    vector<string> v(n);
+    cout<<"Enter values :";
+    for(i=0;i<n;i++){
+        cin>>v[i];
+    }
+    unordered_set<string> mp;
+    vector<string> ar{".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+    for(i=0;i<v.size();i++){
+        string st=v[i];
+        string str;
+        for(j=0;j<st.size();j++){
+            str=str+ar[st[j]-'a'];
+        }
+        if(mp.find(str)==mp.end()){
+            mp.insert(str);
+        }
+    }
+    cout<<mp.size();
+}
