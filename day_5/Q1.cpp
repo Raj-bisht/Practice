433f0fba4cf020aa17851edd37f48be0df26efe1
Raj-bisht <61263677+Raj-bisht@users.noreply.github.com>
2021-07-05 19:17:00 +0530

    sum of all subsets XOR total

---
+++ day_5/Q1.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int x=0;
+void solve(vector<int>& v,int k,int n)
+{
+    x=x+n;
+    for(int i=k;i<v.size();i++)
+    {
+        solve(v,i+1,n^v[i]);
+    }
+}
+int main()
+{
+    int i,j,n;
+    cout<<"Enter size of array :";
+    cin>>n;
+    vector<int> v(n);
+    cout<<"Enter values :";
+    for(i=0;i<n;i++)
+    {
+        cin>>v[i];
+    }
+    for(i=0;i<v.size();i++)
+    {
+        solve(v,i+1,v[i]);
+    }
+    cout<<x;
+}
