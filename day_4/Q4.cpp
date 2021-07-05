e13b9c6e085991cc4b02f25f879f5d6aa977ed06
Raj-bisht <61263677+Raj-bisht@users.noreply.github.com>
2021-07-05 19:24:17 +0530

    correction

--- day_4/Q4.cpp
+++ day_4/Q4.cpp
@@ -1,23 +1,27 @@
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
-vector<int> decode(vector<int>& encoded, int first) {
+vector<int> decode(vector<int>& encoded, int first)
+{
     vector<int> d;
     int x;
     d.push_back(first);
-    for(int i=0;i< encoded.size();i++){
+    for(int i=0;i< encoded.size();i++)
+    {
 
-             x=encoded[i]^first;
+            x=encoded[i]^first;
             first=x;
             d.push_back(first);
     }
     return d;        
 }
-int main(){
+int main()
+{
     vector<int> nums {1,2,3};
     int first=1;
     vector<int> result= decode(nums,first);
-    for(int i=0;i<result.size();i++){
+    for(int i=0;i<result.size();i++)
+    {
         cout<<result[i]<<"  ";
     }
 }
